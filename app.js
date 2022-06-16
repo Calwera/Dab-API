76ef4a4c74b7df72c3d2986ac4a5f4b57510b478
Calwera <calwera@gmail.com>
2022-06-16 21:49:39 +0200

    project configuration

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
-app.get("/api/v1/dab", (req, res) => {
-  res.status(200).json({ message: "DAB is working", app: "DAB API" });
+app.get('/api/v1/dab', (req, res) => {
+  res.status(200).json({ message: 'DAB is working', app: 'DAB API' });
 });
 
-app.post("/api/v1/dab", (req, res) => {
-  res.send("Done");
+app.post('/api/v1/dab', (req, res) => {
+  res.send('Done');
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
-});
+module.exports = app;
